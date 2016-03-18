916890900ab3e0ed76df1dac895badb8cc1330b6
Nikhil George <nikhilgeo@gmail.com>
2016-03-19 02:01:49 +0530

    x-frame-option check done

--- dwn.py
+++ dwn.py
@@ -1,26 +1,21 @@
 from bs4 import BeautifulSoup
-import urllib.request as urllib2
+import requests
 
 
 linklist=[]
 add_flag = False
-URL = "http://jamsheed.in/down/torrents/Outsourced%20Season%201%20Complete%20720p/"
-data = urllib2.urlopen(URL)
-soup = BeautifulSoup(data.read())
+url = "https://www.google.com"
+r= requests.get(url)
+data = r.text
+soup = BeautifulSoup(data)
 soup.find_all('a')
 for link in soup.find_all('a'):
 	#print(link.get('href'))
 	href = link.get('href')
-	if(".mkv" in href):
-		add_flag = True
-	if(".avi" in href):
-		add_flag = True
-	if(".srt" in href):
-		add_flag = True
-	if(".mp4" in href):
-		add_flag = True
-	if (add_flag == True):
-		item_url = URL + href
-		linklist.append(item_url)
-print(linklist)
-
+	if(href.find("http")!=-1):
+		r2=requests.get(href)
+		if('x-frame-options' in r2.headers):
+			print('>>'+href)
+			print('x-frame-options:'+r2.headers['x-frame-options'])
+		else:
+			print("\033[91m {}\033[00m" .format('>>'+href, 1))
