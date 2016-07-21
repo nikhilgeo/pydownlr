1b59d28e21d7e2a6e106b28144139ac19c48cbcd
Nikhil George <nikhilgeo@users.noreply.github.com>
2016-07-21 10:11:34 +0530

    Create kabali.py

---
+++ kabali.py
@@ -0,0 +1,31 @@
+
+#!/usr/bin/env python
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+
+linklist=[]
+add_flag = False
+url = "https://in.bookmyshow.com/buytickets/kabali-bengaluru/movie-bang-ET00039091-MT/20160722"
+r= requests.get(url)
+data = r.text
+soup = BeautifulSoup(data)
+soup.find_all('a')
+for link in soup.find_all('a',{'class':'__venue-name'}):
+                theater = link.getText()
+                if(theater.strip(' \t\n\r') == "Cinepolis: Bannerghatta Road"):
+                        print "adasdasdA"
+                        fromaddr = "tcheck@vmware.com"
+                        toaddr = "nikhilgeorge@vmware.com,rrobert@vmware.com"
+                        msg = MIMEMultipart()
+                        msg['From'] = fromaddr
+                        msg['To'] = toaddr
+                        msg['Subject'] = "KCBR"
+                        body = "Kabali !!"
+                        msg.attach(MIMEText(body, 'plain'))
+                        s = smtplib.SMTP('localhost')
+                        s.sendmail(fromaddr,toaddr.split(','), msg.as_string())
+                        s.quit()
